06c508c7e037d7c0ebc7b79f476574da94fe44f3
mariogmarq <55979398+mariogmarq@users.noreply.github.com>
2019-12-29 18:42:32 +0100

    Update tweepyBot.py

--- tweepyBot.py
+++ tweepyBot.py
@@ -7,17 +7,17 @@
 from FuncionesTweetbot import *
 from time import sleep
 #Array that store users
-users = UserList("usuarios.txt")
+users = UserList(UserTextfile)
     
 #Array with text answers
-posible_answers = AddAnswer("frases.txt")
+posible_answers = AddAnswer(PhrasesTextFile)
 while(True):
-    DMAddPhrase("frases.txt", "1189652909316808705")
-    posible_answers = AddAnswer("frases.txt")
+    DMAddPhrase(PhrasesTextFile, BotId)
+    posible_answers = AddAnswer(PhrasesTextFile)
     for user in users:
         try:
             if NewTweet(user):
                 Answer(user, posible_answers)
         except:
-            api.send_direct_message("1050514751242014721", "Error en User con nombre " + user.name)
+            api.send_direct_message(YourPersonalID, "Error en User con nombre " + user.name)
     sleep(300)
